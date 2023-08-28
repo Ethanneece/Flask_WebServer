a8c172b27e49c872629481dad6b1bc86d99d7fc9
ethann <ethann@vt.edu>
2023-08-28 13:59:12 -0400

    Added logging functionality

--- app.py
+++ app.py
@@ -2,8 +2,12 @@ from flask import Flask
 from flask import send_file
 
 import calculations
+import logging
 
 app = Flask(__name__)
+
+# displays info logs when running. 
+app.logger.setLevel(logging.INFO)
 
 # File name of the image that is sent. 
 OUTPUT_IMAGE_NAME = '3D-Concentration-Display.png'
@@ -12,21 +16,25 @@ OUTPUT_IMAGE_NAME = '3D-Concentration-Display.png'
 
 @app.route('/get-mean', methods=['GET'])
 def getMean():
+    app.logger.info('get-mean called')
 
     return str(calculations.getMean())
 
 @app.route('/get-std-deviation', methods=['GET'])
 def getStd():
+    app.logger.info('get-std-deviation called')
 
     return str(calculations.getStd())
 
 @app.route('/get-sum', methods=['GET'])
 def getSum():
+    app.logger.info('get-sum called')
 
     return str(calculations.getSum())
 
 @app.route('/get-image', methods=['GET'])
 def getImage():
+    app.logger.info('get-Image called')
 
     return send_file(calculations.getImagePath(), OUTPUT_IMAGE_NAME)
 
